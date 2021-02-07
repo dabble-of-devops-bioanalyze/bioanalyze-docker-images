113a8510573828e24eae884b021b9b8dad64a17f
Jillian Rowe <jillian.e.rowe@gmail.com>
2021-02-07 11:22:49 +0300

    test

--- run-cookiecutter.py
+++ run-cookiecutter.py
@@ -11,10 +11,14 @@ TEMPLATE = os.path.join(TEMPLATE, 'templates')
 with open('templates/cookiecutter.json', 'r') as reader:
     data = json.load(reader)
 
-cookiecutter(
-    TEMPLATE,  # path/url to cookiecutter template
-    overwrite_if_exists=True,
-    extra_context=data,
-    output_dir='.',
-    no_input=True
-)
+for pangeo_version in data['pangeo_versions']:
+
+    data['pangeo_notebook_version'] = pangeo_version
+
+    cookiecutter(
+        TEMPLATE,  # path/url to cookiecutter template
+        overwrite_if_exists=False,
+        extra_context=data,
+        output_dir='biohub-docker-images',
+        no_input=True
+    )
